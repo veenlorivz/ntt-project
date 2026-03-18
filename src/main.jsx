11f75c919183913fbf38ba7b12d71ba0d24449d9
veenlorivz <veenlorivz194@gmail.com>
2026-03-18 21:39:38 +0700

    Fix base url

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename='/ntt-project'>
         <App />
       </BrowserRouter>
     </Provider>
